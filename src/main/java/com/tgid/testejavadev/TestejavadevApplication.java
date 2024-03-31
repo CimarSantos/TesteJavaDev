7b3b6bce3db226914a1117a260a60d52ab321e9c
CimarSantos <francimarlsantos@outlook.com>
2024-03-31 18:19:04 -0300

    Projeto com Spring Boot

---
+++ src/main/java/com/tgid/testejavadev/TestejavadevApplication.java
@@ -0,0 +1,13 @@
+package com.tgid.testejavadev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestejavadevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestejavadevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tgid/testejavadev/TestejavadevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgid.testejavadev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestejavadevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
